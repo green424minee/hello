0aaf3706758c136dced41d616be85359193ab301
SYacuCLoud <syacucloud@naver.com>
2022-06-29 10:19:48 +0900

    연습 중~~

--- 220629-01 git/src/Main.java
+++ 220629-01 git/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 	public static void main(String[] args) {
-		// 헬로 월드 완성
-		System.out.println("Hello World");
+		/*
+		 * Push 연습
+		 */
+		System.out.println("Push Push");
 	}
 }
